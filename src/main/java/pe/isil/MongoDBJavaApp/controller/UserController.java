5d71ffad8f62679f25f993edf9f8fd4e18289138
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-07-12 22:28:28 -0500

    Add files via upload

---
+++ src/main/java/pe/isil/MongoDBJavaApp/controller/UserController.java
@@ -0,0 +1,57 @@
+package pe.isil.MongoDBJavaApp.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import pe.isil.MongoDBJavaApp.model.User;
+import pe.isil.MongoDBJavaApp.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id){
+        return userRepository.findById(id);
+    } 
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping
+    public void deleteUser(@PathVariable String id) {
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/search")
+    public List<User> getUserByName(@RequestParam String name) {
+        return userRepository.findByName(name);
+    }
+}
